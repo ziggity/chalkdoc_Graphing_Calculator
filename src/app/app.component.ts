1bf88dd5617979fc98e72a315cd09e8cb335391a
ziggity <zachbeecher@gmail.com>
2017-08-31 15:36:21 -0700

    function plot works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+declare let d3: any;
+declare let functionPlot: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    console.log( 'Onit' )
+    let ele = d3.selectAll('p')
+
+    ele.style('background-color', 'red')
+
+    functionPlot({
+      target: '#quadratic',
+      data:[{
+        fn: 'x=1'
+      }]
+    })
+  }
 }
+
